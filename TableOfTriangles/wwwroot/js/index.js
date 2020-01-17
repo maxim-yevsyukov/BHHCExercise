8859918e18d86c2c54adbe6823c98f3a6cd602e7
maxim-yevsyukov <maxim.yevsyukov@gmail.com>
2020-01-17 13:17:18 -0800

    Minor visual corrections

--- TableOfTriangles/wwwroot/js/index.js
+++ TableOfTriangles/wwwroot/js/index.js
@@ -59,26 +59,17 @@ function _displayItems(data) {
 
     canvas.on('mouse:down', canvasMouseDown);
 
-    console.log("how many times _displayItems");
-
-    console.log(data);
-
     data.forEach(item => {
 
         var p1 = { x: item.v1.x * visualMultiplier, y: item.v1.y * visualMultiplier },
             p2 = { x: item.v2.x * visualMultiplier, y: item.v2.y * visualMultiplier },
             p3 = { x: item.v3.x * visualMultiplier, y: item.v3.y * visualMultiplier };
 
-        var textLeft = (item.column % 2 == 0) ? p1.x + (p3.x - p1.x) / 2 : p1.x + (p3.x - p1.x) / 4;
-        var textTop = (item.column % 2 == 0) ? p1.y + (p3.y - p2.y) / 4 : p2.y - (p2.y - p1.y) / 3;
+        var textLeft = (item.column % 2 === 0) ? p1.x + (p3.x - p1.x) / 3 : p1.x + (p3.x - p1.x) / 9;
+        var textTop = (item.column % 2 === 0) ? p1.y + (p3.y - p2.y) / 8 : p2.y - (p2.y - p1.y) / 3;
 
         textLeft = Math.round(textLeft);
         textTop = Math.round(textTop);
-
-        console.log(item.row+item.column + ": ");
-        console.log(p1.x + " " + p1.y + ", " + p2.x + " " + p2.y + ", " + p3.x + " " + p3.y);
-        console.log("textLeft: " + textLeft);
-        console.log("textTop: " + textTop);
 
         var text = new fabric.Text(item.reason, { left: textLeft, top: textTop, fontSize: fontSize });
         text.setColor(white);
